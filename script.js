da7f4a79d458c411c891fd53ac670b585293b0e8
Adam M. Vaughn <adammathewvaughn@gmail.com>
2022-10-13 22:11:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,50 @@ export function currentTime() {
 new Date().getFullYear();
 return year
 }
+export function innovateDiv() {
+    let innovateLink =
+window.open("https://github.com/adammathewvaughn?tab=repositories", "_blank", "noreferrer")
+return innovateLink
+}
+export function bashDiv() {
+    let bashLink =
+window.open("https://github.com/adammathewvaughn/Bash", "_blank", "noreferrer")
+return bashLink
+}
+export function logoDiv() {
+    let logoLink =
+window.open("https://adammathewvaughn.com", "_blank", "noreferrer")
+return logoLink
+}
+export function riscDiv() {
+    let riscLink =
+window.open("https://riscv.org", "_blank", "noreferrer")
+return riscLink
+}
+export function pythonDiv() {
+    let pythonLink =
+window.open("https://github.com/adammathewvaughn/ib_python_course", "_blank", "noreferrer")
+return pythonLink
+}
+export function rpiDiv() {
+    let rpiLink =
+window.open("https://www.raspberrypi.com", "_blank", "noreferrer")
+return rpiLink
+}
+export function vueDiv() {
+    let vueLink =
+window.open("https://vuejs.org", "_blank", "noreferrer")
+return vueLink
+}
+export function dbDiv() {
+    let dbLink =
+window.open("https://adammathewvaughn.com", "_blank", "noreferrer")
+return dbLink
+}
+export function ciscoDiv() {
+    let ciscoLink =
+window.open("https://adammathewvaughn.com", "_blank", "noreferrer")
+return ciscoLink
+}
+
+
